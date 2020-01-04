24a92869081f6bccf308f635d5a9c7feef67cc2b
ZoZTravolta <zoharsaghi@gmail.com>
2020-01-04 11:57:56 +0200

    first commit

---
+++ botoReplyFunctions.py
@@ -0,0 +1,147 @@
+import words
+import random
+import time
+import requests
+import keys
+import json
+
+swearCounter = -1
+noUnderstandCounter = -1
+userName = ''
+
+
+def randomNum(listToRandom):
+    return random.randint(0, len(listToRandom) - 1)
+
+
+def removeUselessWords(user_message):
+    querywords = user_message.split()
+    newQuery = []
+    for word in querywords:
+        if word not in words.wordsToRemove:
+            newQuery.append(word)
+
+    newQuery = ' '.join(newQuery)
+    return newQuery
+
+
+def cleanString(user_message):
+    querywords = user_message.split()
+    newQuery = []
+    for word in querywords:
+        if word not in words.wordsToRemove and word not in words.questions:
+            word = word[:-1] if word.endswith('?') else word
+            newQuery.append(word)
+    newQuery = ' '.join(newQuery)
+    return newQuery
+
+
+def getJoke():
+    joke = requests.get('http://api.icndb.com/jokes/random/')
+    joke = joke.json()
+    return joke['value']['joke'] + '   ha ha ha, this api is so funny!'
+
+
+def searchWiki(user_message):
+    toList = user_message.split(' ')
+    toQuery = '_'.join(toList)
+    response = requests.get(
+        'https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=' + toQuery)
+    responseJson = response.json()
+    key = responseJson['query']['pages'].keys()
+    key = list(key)
+    if key[0] == '-1':
+        return 'Sorry could not find this in wikipedia'
+    else:
+        article = responseJson['query']['pages'][key[0]]['extract']
+        # print(responseJson['query']['pages'][key[0]]['extract'])
+        index = article.find('.')
+        article = article[:index+1]
+        # print(article)
+        return article
+
+
+#searchWiki(cleanString('what is a ffff?'))
+
+
+def handleQuestions(user_message):
+    if 'you' in user_message:
+        return talkingAboutBoto(user_message), 'inlove'
+    if 'me' in user_message or 'i ' in user_message:
+        return talkingAboutUser(user_message), 'inlove'
+    if 'weather' in user_message:
+        return getWeather(cleanString(user_message))
+    else:
+        return searchWiki(cleanString(user_message)), 'takeoff'
+
+
+def talkingAboutUser(user_message):
+    return 'talking about user'
+
+
+def talkingAboutBoto(user_message):
+    if 'who' in user_message:
+        return "My name is boto, i'm here to help"
+    if 'what' in user_message:
+        return "I'm just a bot actually"
+
+
+def getWeather(user_message):
+    #user_message = removeUselessWords(user_message)
+
+    api_key = keys.weatherMapApiKey
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    city_name = user_message
+    complete_url = base_url + "appid=" + api_key + \
+        "&q=" + city_name + '&units=metric'
+    response = requests.get(complete_url)
+    responseJson = response.json()
+
+    if responseJson["cod"] != "404":
+        y = responseJson["main"]
+        current_temperature = y["temp"]
+        current_humidiy = y["humidity"]
+        return 'The weather in ' + user_message + ' is: ' + str(current_temperature) + '° celsius and humidity is ' + str(current_humidiy) + '%', 'inlove'
+
+    else:
+        return 'Sorry city Not Found. The sintax should be like: "what is the weather in tel aviv"', 'crying'
+
+
+def checkAnswer(user_message):
+    global userName
+    global swearCounter
+    global noUnderstandCounter
+
+    stringAfterCleaning = removeUselessWords(user_message)
+    ######################boto do handeling swear words ###########################
+
+    if swearCounter == 2:
+        return 'boto is mad at you, and not talking to you any more!', 'crying'
+    if any(word in words.user_swear for word in user_message.split(' ')):
+        swearCounter += 1
+        return (userName + ' ' + words.boto_angry[swearCounter]), 'no'
+    ######################boto reply to greetings ###########################
+    if not userName:
+        userName = stringAfterCleaning
+        return 'nice to meet you ' + stringAfterCleaning + ' what do you want to ask me?', 'excited'
+
+    if any(word in words.user_greet for word in user_message.split(' ')):
+        return words.boto_greet[randomNum(words.boto_greet)], 'excited'
+
+    ######################boto tells a joke ###########################
+
+    if 'joke' in user_message:
+        return getJoke(), 'giggling'
+
+    ######################boto handels questions ###########################
+
+    if user_message.endswith('?') or any(word in words.questions for word in user_message.split(' ')):
+        return handleQuestions(user_message)
+
+    ######################boto do not anderstand ###########################
+    else:
+        if noUnderstandCounter == 2:
+            noUnderstandCounter = -1
+        else:
+            noUnderstandCounter += 1
+        return words.boto_notUnderstand[noUnderstandCounter], 'confused'
---
+++ words.py
@@ -0,0 +1,49 @@
+
+wordsToRemove = ['is', 'at', 'is',
+                 'he', 'the', 'in', 'tell', 'me', 'my', 'name', 'a', 'an']
+
+questions = ['what', 'where', 'whats', 'weather', '?']
+###############################################################################
+
+user_greet = ['hello', 'good', "what's up",
+              "what's new", 'how are you', 'okay', 'hey', 'hi']
+
+user_swear = ['anal', 'anus', 'arse', 'ass', 'ballsack', 'balls', 'bastard', 'bitch', 'biatch', 'bloody', 'blowjob', 'blow job', 'bollock', 'bollok', 'boner', 'boob', 'bugger', 'bum', 'butt', 'buttplug', 'clitoris', 'cock', 'coon', 'crap', 'cunt', 'damn', 'dick', 'dildo', 'dyke', 'fag', 'feck', 'fellate', 'fellatio', 'felching', 'fuck', 'f u c k', 'fudgepacker',
+              'fudge packer', 'flange', 'Goddamn', 'God damn', 'hell', 'homo', 'jerk', 'jizz', 'knobend', 'knob end', 'labia', 'lmao', 'lmfao', 'muff', 'nigger', 'nigga', 'omg', 'penis', 'piss', 'poop', 'prick', 'pube', 'pussy', 'queer', 'scrotum', 'sex', 'shit', 's hit', 'sh1t', 'slut', 'smegma', 'spunk', 'tit', 'tosser', 'turd', 'twat', 'vagina', 'wank', 'whore', 'wtf']
+
+###############################################################################
+
+boto_greet = [
+    "Hello! This is Boto, Don’t be afraid to ask stupid questions. I love them!", 'Hi you!', 'hello!']
+
+boto_angry = ["Please don't say that!",
+              "this is your second strike, 3 you are out!", 'you are over the line, bye bye...']
+
+boto_notUnderstand = ["Sorry can you speak English?", "Did you smoke something?",
+                      "I have no idea what you are talking about. Try to ask me question like...:", ]
+
+
+# USER_MEAN = ["I thought you'd be more fun", "This sucks", "Do you have nothing else to say",
+#              "KK", "Anything else to say?", "You've already said that", "And what"]
+# HURT_BOT = ["Look, I'm sorry you're bored, I'm a basic chatbot created by some 26 year old. Please be gentle with me",
+#             "You don't have to be mean! Robots have feelings too.", "Oi! Don't be so aggressive.", "YOLO"]
+
+# # if user thinks robot is being mean
+# USER_HURT = ["rude", "relax", "mean", "fisty one today Robo"]
+# BOT_APOLOGY = ["why you so sensitive, LMAO", "grow up, real life will be like this",
+#                "I apologize, can we move on?", "Lets be friends please"]
+
+# # if user wants to move on
+# USER_CONVO = ["sorry", "yes", "nothing new",
+#               "no gossip", "that's fun", "im good", "good", "ok"]
+# BOT_CONVO_ANSWER = ["Ok moving on, what do you want to talk about now?",
+#                     "Can we move to the next topic please?", "Yalla, Yalla"]
+
+# # if user response is yes or no
+# YES_NO = ["yes", "no", "yep", "yup", "nope"]
+# BOT_YES_NO = ["But like same though, let's talk about something else",
+#               "I feel the exact same way! Ask me to do something robots do", "I can do cool things too you know"]
+
+# # if user asks a question
+# USER_QUESTIONMARK = ["That's pretty personal",
+#                      "How would you feel if I asked you a question like that?", "That's too deep", "I have my limits!"]
